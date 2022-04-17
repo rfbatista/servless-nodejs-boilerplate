7cc997e30098b191babda4bb35f6ce03dae15904
rfbatista <renfbatista@gmail.com>
2022-04-17 11:14:49 -0300

    initial config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+    'quotes': ['error', 'single'],
+    'symbol-description': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-nested-ternary': 'warn',
+    'import/no-unresolved': 0,
+  },
+};
